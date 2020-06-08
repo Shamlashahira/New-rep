07a3aa5ccb08a0866d448fdd7606f3b857fa0dd5
Shamlashahira <66350438+Shamlashahira@users.noreply.github.com>
2020-06-08 20:42:19 +0530

    Add files via upload

---
+++ day10work/Animal.java
@@ -0,0 +1,16 @@
+package day10work;
+
+public class Animal {
+	
+	void eat() {
+		System.out.println("All animals eat");
+	}
+
+	void sleep() {
+		System.out.println("All animals sleep");
+	}
+	void move() {
+		
+		System.out.println("Not sure how it will move yet.....");
+	}
+}
---
+++ day10work/Snake.java
@@ -0,0 +1,10 @@
+package day10work;
+
+public class Snake extends Animal{
+	
+	@Override
+	void move() {
+		System.out.println("Snakes crawl.....");
+	}
+
+}
